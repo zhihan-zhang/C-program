6d8df9ee4c96cd71b18a8f7d17100fe787df496e
Zhihan <zzh@qq.com>
2020-04-29 20:42:24 +0800

    Modified one line

---
+++ testgit.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: testgit.c
+	> Author: 
+	> Mail: 
+	> Created Time: Wed 29 Apr 2020 08:33:57 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+int main(){
+
+    printf("branch and delete branch,nothing to remember");
+    return 0;
+}
